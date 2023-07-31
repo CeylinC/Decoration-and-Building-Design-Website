06ac4cbcecbc639fe21a24759090cd216695cf4f
Ceylin <caltepeceylin@gmail.com>
2023-07-31 21:55:33 +0300

    refactor: minor change

--- decoration-build-design/src/components/navbar/Navbar.jsx
+++ decoration-build-design/src/components/navbar/Navbar.jsx
@@ -4,21 +4,21 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
-    <nav class="navbar">
-        <div class="navbar-container container">
+    <nav className="navbar">
+        <div className="navbar-container container">
             <input type="checkbox" name="" id="" />
-            <div class="hamburger-lines">
-                <span class="line line1"></span>
-                <span class="line line2"></span>
-                <span class="line line3"></span>
+            <div className="hamburger-lines">
+                <span className="line line1"></span>
+                <span className="line line2"></span>
+                <span className="line line3"></span>
             </div>
-            <ul class="menu-items">
+            <ul className="menu-items">
                 <li><a href="#">Anasayfa</a></li>
                 <li><a href="#">Hakkımızda</a></li>
                 <li><a href="#">Projeler</a></li>
                 <li><a href="#">İletişim</a></li>
             </ul>
-            <h1 class="logo">Yapı<span>Tasarım</span><span>Dekorasyon</span></h1>
+            <h1 className="logo">Yapı<span>Tasarım</span><span>Dekorasyon</span></h1>
         </div>
     </nav>
   );
