a2f45099786f9555874ac96190e2586596f3bb62
Ceylin <caltepeceylin@gmail.com>
2023-08-02 02:20:21 +0300

    style: change opacity of slider

--- decoration-build-design/src/components/slider/Slider.jsx
+++ decoration-build-design/src/components/slider/Slider.jsx
@@ -1,6 +1,5 @@
 import './Slider.css';
 import Carousel from 'react-bootstrap/Carousel';
-import Button from 'react-bootstrap/Button';
 
 function Slider() {
   return (
@@ -14,7 +13,6 @@ function Slider() {
         <Carousel.Caption className='slider-one'>
           <h1>Evinizin İhtişamını Keşfedin</h1>
           <p>Estetik ve fonksiyonu bir araya getiren tasarımlarımızla evinizi dönüştürün.</p>
-          <Button href="#" variant="dark">Daha Fazlasını Öğren</Button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -25,7 +23,6 @@ function Slider() {
            <Carousel.Caption className='slider-two'>
           <h1>Zarafetin Sırrı</h1>
           <p>İnce detaylarda gizli olan zarif tasarımlarımızla mekanlarınıza karakter katın.</p>
-          <Button href="#" variant="dark">Daha Fazlasını Öğren</Button>
          </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -37,7 +34,6 @@ function Slider() {
         <Carousel.Caption className='slider-three'>
          <h1>Kişiselliğinizi Yansıtın</h1>
          <p>Size özel tasarımlarımızla evinizi kendi tarzınıza uygun hale getirin.</p>
-         <Button href="#" variant="dark">Daha Fazlasını Öğren</Button>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
